773ba590eb8c33c9283d94623080d1e4e3aff229
Kevin Christian <kevin.c23@mhs.istts.ac.id>
2025-05-12 16:24:35 +0700

    hapus urlended

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 
 const route = require("./src/routes/route");
-const { urlencoded } = require("body-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
