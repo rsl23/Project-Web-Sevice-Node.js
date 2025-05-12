40ad73a67651c23bc8c129a52e7a87a80306d042
Kevin Christian <kevin.c23@mhs.istts.ac.id>
2025-05-12 17:08:20 +0700

    login

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,5 +1,7 @@
 const { Op, Sequelize } = require("sequelize");
 const { User } = require("../models/fetchModel");
+const jwt = require("jsonwebtoken");
+const JWT_KEY = "ProyekWS";
 
 const register = async (req, res) => {
   try {
@@ -22,4 +24,37 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const findUser = await User.findOne({
+      where: { username: username },
+    });
+    if (findUser) {
+      if (findUser.password !== password) {
+        return res.status(400).json({ message: "Password Salah" });
+      } else {
+        const token = jwt.sign(
+          {
+            username: username,
+          },
+          JWT_KEY,
+          {
+            expiresIn: "24h",
+          }
+        );
+        return res.status(200).json({
+          message: `Selamat ${username} berhasil melakukan login!`,
+          token: token,
+        });
+      }
+    } else {
+      return res.status(404).json({ message: "User tidak ditemukan" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { register, login };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { register } = require("../controller/controller");
+const { register, login } = require("../controller/controller");
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
