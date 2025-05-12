dbda1a5a3cf890a31a0b769f2834b875b4eb1ce4
Kevin Christian <kevin.c23@mhs.istts.ac.id>
2025-05-12 16:51:33 +0700

    Register Berhasil

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -4,14 +4,22 @@ const { User } = require("../models/fetchModel");
 const register = async (req, res) => {
   try {
     const { username, password, name } = req.body;
+    const time = new Date();
 
     const query = await User.create({
       username: username,
       password: password,
       name: name,
       saldo: 0,
+      createdAt: time,
+      updatedAt: time,
     });
+    return res
+      .status(200)
+      .json({ message: `Berhasil Register dengan Usernmae ${username}` });
   } catch (err) {
-    console.log(err.message);
+    res.status(500).json({ message: err.message });
   }
 };
+
+module.exports = { register };
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,4 +2,6 @@ const express = require("express");
 const { register } = require("../controller/controller");
 const router = express.Router();
 
-router.get("/register", register);
+router.post("/register", register);
+
+module.exports = router;
