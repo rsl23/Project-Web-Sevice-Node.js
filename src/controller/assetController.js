f8635eb9c066b4385f26395f8184870bf20bee90
Kevin Christian <kevin.c23@mhs.istts.ac.id>
2025-05-12 19:43:47 +0700

    Get assets

--- src/controller/assetController.js
+++ src/controller/assetController.js
@@ -1,2 +1,24 @@
 const { Op, Sequelize } = require("sequelize");
-const { User } = require("../models/fetchModel");
+const axios = require("axios");
+
+const fetchCoin = async (req, res) => {
+  try {
+    const url = "https://api.coingecko.com/api/v3/coins/list";
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-pro-api-key": "CG-Difbd7y1YycHVAVvyUDzocDu",
+      },
+    };
+
+    const response = await axios(url, options);
+    console.log(response.data);
+    return res.status(200).json({ Assets: response.data });
+  } catch (err) {
+    console.error("Error response:", err.response?.data || err.message);
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { fetchCoin };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { register, login, profile } = require("../controller/authController");
 const { verifyToken } = require("../middleware/authMiddleware");
+const { fetchCoin } = require("../controller/assetController");
 const router = express.Router();
 
 router.post("/auth/register", register);
 router.post("/auth/login", login);
 router.get("/auth/me", verifyToken, profile);
+
+router.get("/assets", fetchCoin);
 
 module.exports = router;
