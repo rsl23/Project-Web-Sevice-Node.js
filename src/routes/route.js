01af8139c4528669c0abd96dc92eaefff8907bd9
Kevin Christian <kevin.c23@mhs.istts.ac.id>
2025-05-13 20:06:28 +0700

    merge

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,32 +1,30 @@
 const express = require("express");
-const { register, login, profile, requestPasswordReset, updatePassword } = require("../controller/authController");
+const {
+  register,
+  login,
+  profile,
+  requestPasswordReset,
+  updatePassword,
+} = require("../controller/authController");
 const { verifyToken } = require("../middleware/authMiddleware");
 const { fetchCoin, detailAsset } = require("../controller/assetController");
-<<<<<<< HEAD
 const { BuyTransaction } = require("../controller/transactionController");
-=======
-const { price, coinPrice, marketTrending } = require("../controller/priceController");
->>>>>>> ef77066d39796436aaf9e11446daeec5ec985560
 const router = express.Router();
 
+//==================================== LOGIN & REGISTER ==================================================
 router.post("/auth/register", register);
 router.post("/auth/login", login);
 router.get("/auth/me", verifyToken, profile);
 
-// -----------------------------------------------------------------------
 router.post("/auth/request_verification", requestPasswordReset);
 router.post("/auth/reset_password", updatePassword);
-// -----------------------------------------------------------------------
 
+//===================================== ASSET ==============================================================
 router.get("/assets", fetchCoin);
 router.get("/assets/:id", detailAsset);
 
-<<<<<<< HEAD
+//=========================================== TRANSACTION =====================================================
+
 router.post("/transaction/buy", verifyToken, BuyTransaction);
-=======
-router.get("/prices", price);
-router.get("/prices/:coin", coinPrice);
-router.get("/market/trending", marketTrending);
->>>>>>> ef77066d39796436aaf9e11446daeec5ec985560
 
 module.exports = router;
